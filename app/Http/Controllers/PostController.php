5960cfefa60ab40cdb63fffa91f4ac072c2c735f
tangbim <santosolintang@gmail.com>
2021-11-22 17:44:30 +0700

    change partial and routing

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,19 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
-class PostController extends Controller
-{
-    public function index()
-    {
+class PostController extends Controller {
+    public function index() {
         return view('posts', [
             "title" => "Posts",
             "posts" => Post::all()
         ]);
     }
 
-    public function show(Post $post)
-    {
-        return view('partials/post', [
+    public function show(Post $post) {
+        return view('post', [
             "title" => "Single Post",
             "post" => $post
         ]);
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,8 +2,8 @@
 
 @section('container')
 
-<h1>Halaman About</h1>
-<h3>{{$name;}}</h3>
-<p>{{$email}}</p>
-<img src="img/{{$images}}" alt="{{$name}}" width="200" class="img-thumbnail rounded-circle">
+    <h1>Halaman About</h1>
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $images }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
 @endsection
