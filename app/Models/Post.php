ebf0ec03cc8605b1beb76f6a9f3c2edaa515e7fb
tangbim <santosolintang@gmail.com>
2021-11-22 22:04:05 +0700

    initial commit from video 9

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,15 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
-{
+class Post extends Model {
     use HasFactory;
 
     protected $guarded = ['id'];
     protected $fillable = ['title', 'slug', 'excerpt', 'body', 'category_id'];
 
-    public function category()
-    {
+    public function category() {
         return $this->belongsTo(Category::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,10 +5,12 @@
     <article>
         <h1 class="mb-5">{{ $post->title }}</h1>
 
-        <p>By: Lintang Bima in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By: <a href="" class="text-decoration-none">{{ $post->user->name }}</a> in <a
+                href="/categories/{{ $post->category->slug }}"
+                class="text-decoration-none">{{ $post->category->name }}</a></p>
 
         {!! $post->body !!}
     </article>
 
-    <a href="/blog">Back to Posts</a>
+    <a href="/posts" class="text-decoration-none">Back to Posts</a>
 @endsection
